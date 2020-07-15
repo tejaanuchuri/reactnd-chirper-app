056ef3c44c662a1e7a42d724a8dbe4cdaeb3448b
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-15 23:09:17 +0530

    "install redux ,react-redux and create a store in src/index.js file .the store encoded with provider"

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -2,10 +2,10 @@ import { RECEIVE_TWEETS } from  '../actions/tweets'
 
 export default function users (state={}, actions) {
     switch(actions.type) {
-        case RECEIVE_tweets:
+        case RECEIVE_TWEETS:
             return {
                 ...state,
-                ...action.tweets
+                ...actions.tweets
             }
         default :
             return state;

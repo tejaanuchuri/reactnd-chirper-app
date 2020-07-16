f06edc45cce0718005a9c8dd9e0e3b5d82c18f78
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-16 13:50:57 +0530

    "create a store and install reducer"

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-ReactDOM.render(<App />, document.getElementById('root'))
+import { createStore } from 'redux'
+import { provider, Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider>
+<App /></Provider>, document.getElementById('root'))
